685b26cf6336aad4ad0d679afa093cf15dba6f74
edwindelossantos08 <20251032@itla.edu.do>
2025-09-25 18:29:31 -0400

    Tarea #1
    Agregue DatosPersonales

---
+++ DatosPersonales/src/datospersonales/DatosPersonales.java
@@ -0,0 +1,13 @@
+
+package datospersonales;
+
+public class DatosPersonales {
+
+  
+    public static void main(String[] args) {
+        System.out.println("Edwin Alexander De Los Santos PEralta");
+        System.out.println("20251032@itla.edu.do");
+    }
+          
+    
+}
