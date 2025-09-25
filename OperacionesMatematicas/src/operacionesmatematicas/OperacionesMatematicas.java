d183621932f609090cc6a3dff3911cd547cdc1de
edwindelossantos08 <20251032@itla.edu.do>
2025-09-25 18:34:19 -0400

    Tarea #1
    Agregue Operaciones Matematicas

---
+++ OperacionesMatematicas/src/operacionesmatematicas/OperacionesMatematicas.java
@@ -0,0 +1,27 @@
+
+package operacionesmatematicas;
+
+public class OperacionesMatematicas {
+
+    public static void main(String[] args) {
+        //2. Realizar un programa que incluya las 4 operaciones matemáticas básicas (Suma, Resta, Multiplicación y División).
+       
+       //Fixed values
+       int num1 = 12;
+       int num2 = 8;
+       
+       //Operations
+       int sum = num1 + num2;
+       int difference = num1 - num2;
+       int product = num1 * num2;
+       int quotient = num1 / num2;
+       
+      //Display results
+      System.out.println("Numbers: " + num1 + " and " + num2);
+      System.out.println("Sum: " + sum);
+      System.out.println("Difference: " + difference);
+      System.out.println("Product: " + product);
+      System.out.println("Quotient: " + quotient);
+       
+ }
+}
