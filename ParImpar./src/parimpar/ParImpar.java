b97abefa50311f12a1712bb59383995400cc5e83
edwindelossantos08 <20251032@itla.edu.do>
2025-09-26 20:04:37 -0400

    Tarea #1
    Agregue esta nuevamente porque la había hecho con validación de entrada.

---
+++ ParImpar./src/parimpar/ParImpar.java
@@ -0,0 +1,18 @@
+
+package parimpar;
+
+public class ParImpar {
+
+    public static void main(String[] args) {
+        //8. Realizar un programa que ingresado un numero, me indique si es par o impar.
+        
+        int num = 13;
+        
+        if (num % 2 == 0){
+            System.out.println(num + " es un numero par."); 
+        }else{
+            System.out.println(num + " es un numero impar.");
+        }
+    }
+    
+}
